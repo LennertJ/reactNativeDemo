1283318b5424438430982dd7a714b7fa916a63a7
LennertJ <jans.lennert@gmail.com>
2022-01-22 16:40:03 +0100

    basic Flatlist of spells

--- components/spellslist/spellslist.js
+++ components/spellslist/spellslist.js
@@ -1,14 +1,49 @@
 import React, { Component } from 'react';
-import { View,Text } from "react-native";
 import { baseStyles } from "../../styles/base";
+import { ActivityIndicator, FlatList, Text, View } from 'react-native';
 
 class Spellslist extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            data: [],
+            isLoading: true
+        };
+    }
+
+    componentDidMount() {
+        this.getSpells();
+    }
+
+    async getSpells() {
+        try {
+            const response = await fetch('https://www.dnd5eapi.co/api/spells/');
+            const json = await response.json();
+            this.setState({ data: json.results });
+        } catch (error) {
+            console.log(error);
+        } finally {
+            this.setState({ isLoading: false });
+        }
+    }
     render() {
+        const { data, isLoading } = this.state;
         return (
-            <View style={baseStyles.background}>
-                <Text>spellslist</Text>
+            <View style={{ flex: 1, padding: 24 }}>
+                
+                {isLoading ? <ActivityIndicator /> : (
+                    <FlatList
+                        data={data}
+                        keyExtractor={({ id }, index) => id}
+                        renderItem={({ item }) => (
+                        <listItem url = {item.url} name={item.name}></listItem>
+                        //<Text>{item.name}, {item.url}</Text>
+                        )}
+                    />
+                )}
             </View>
         );
     }
-} 
+}
 export default Spellslist;
